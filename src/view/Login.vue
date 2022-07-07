4d1582bd15d03da90033bebde733e64ccca3e306
南巷北风 <2263214154@qq.com>
2022-07-07 16:29:25 +0800

    项目环境配置完毕

---
+++ src/view/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+      登录页测试
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Login'
+}
+</script>
+
+<style scoped>
+  .container{
+    height: 100%;
+    /* background-image: linear-gradient(20deg,red,yellow); */
+  }
+</style>
