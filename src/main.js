26a6ae017a0f64b6df501553ef89b3bd56db5b8b
南巷北风 <2263214154@qq.com>
2022-07-13 09:05:45 +0800

    登录注册功能完成

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL='http://127.0.0.1:3000/'
+
+Vue.prototype.$http=axios
+
 
 new Vue({
   router,
--- src/view/Login.vue
+++ src/view/Login.vue
@@ -13,34 +13,66 @@
             <img src="../assets/logo.png" alt="" />
           </div>
         </div>
-        <el-form ref="form" label-width="0px">
-          <el-form-item>
-            <el-input prefix-icon="el-icon-user"></el-input>
+        <el-form ref="form" label-width="0px" :rules="check" :model="rulesForm">
+          <el-form-item prop="username">
+            <el-input
+              prefix-icon="el-icon-user"
+              v-model="rulesForm.username"
+              placeholder="请输入用户名"
+            ></el-input>
           </el-form-item>
-          <el-form-item>
-            <el-input prefix-icon="el-icon-lock"></el-input>
+          <el-form-item prop="password">
+            <el-input
+              prefix-icon="el-icon-lock"
+              v-model="rulesForm.password"
+              placeholder="请输入密码"
+              type="password"
+            ></el-input>
           </el-form-item>
         </el-form>
         <div class="register">
-          <a href="#" @click="show = !show">账户注册</a>
+          <a href="#" @click="Register">账户注册</a>
         </div>
-        <el-button class="login" type="primary">登录</el-button>
+        <el-button class="login" type="primary" @click="Login">登录</el-button>
 
-        <div class="reg_box" v-if="show">
-
-          <h3 style="color:#000">用户注册</h3>
-          <el-form ref="form" label-width="0px">
-            <el-form-item >
-              <el-input prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+        <div class="reg_box" v-show="show">
+          <h3 style="color: #000; margin-bottom: 20px">用户注册</h3>
+          <el-form
+            ref="logon"
+            label-width="0px"
+            :rules="rules"
+            :model="regisForm"
+          >
+            <el-form-item prop="username">
+              <el-input
+                v-model="regisForm.username"
+                prefix-icon="el-icon-user"
+                placeholder="请输入用户名"
+              ></el-input>
             </el-form-item>
-            <el-form-item >
-              <el-input prefix-icon="el-icon-lock" placeholder="请输入密码"></el-input>
+            <el-form-item prop="password">
+              <el-input
+                v-model="regisForm.password"
+                prefix-icon="el-icon-lock"
+                placeholder="请输入密码"
+                type="password"
+              ></el-input>
             </el-form-item>
-            <el-form-item >
-              <el-input prefix-icon="el-icon-lock" placeholder="确认密码"></el-input>
+            <el-form-item prop="confirmPass">
+              <el-input
+                v-model="regisForm.confirmPass"
+                prefix-icon="el-icon-lock"
+                placeholder="确认密码"
+                type="password"
+              ></el-input>
             </el-form-item>
           </el-form>
-            <el-button type="primary" style="width:100%">注册</el-button>
+          <div class="login_btn">
+            <a href="#" @click="Logins">去登陆</a>
+          </div>
+          <el-button type="primary" style="width: 100%" @click="Regi"
+            >注册</el-button
+          >
         </div>
       </div>
     </div>
@@ -51,13 +83,109 @@
 export default {
   name: "Login",
   data() {
+    var validPass = (rulse, value, callback) => {
+      if (value == "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.regisForm.password) {
+        callback(new Error("两次输入密码不一致"));
+      } else {
+        callback();
+      }
+    };
     return {
       show: false,
+      rulesForm: {
+        username: "",
+        password: "",
+      },
+      regisForm: {
+        username: "",
+        password: "",
+        confirmPass: "",
+      },
+      check: {
+        name: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3~5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6~12个字符", trigger: "blur" },
+        ],
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 5, message: "长度在 3~5 个字符", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 12, message: "长度在 6~12个字符", trigger: "blur" },
+        ],
+        confirmPass: [
+          { required: true, message: "请输入确认密码", trigger: "blur" },
+          { validator: validPass, trigger: "blur" },
+        ],
+      },
     };
   },
-  methods: {},
+  methods: {
+    Logins() {
+      this.show = !this.show;
+      this.$refs.logon.resetFields();
+    },
+    Register() {
+      this.show = !this.show;
+      this.$refs.form.resetFields();
+    },
+    // 用户注册
+    Regi() {
+      this.$refs.logon.validate(async (v) => {
+        if (!v) return;
+        const { data: res } = await this.$http.post("/api/reguser", {
+          username: this.regisForm.username,
+          password: this.regisForm.password,
+        });
+        if (res.status !== 200) {
+          this.$message({
+            type: "error",
+            message: res.msg,
+          });
+          return;
+        }
+        this.$message({
+          type: "success",
+          message: res.msg,
+        });
+      });
+    },
+
+    // 用户登录
+    Login() {
+      this.$refs.form.validate(async (v) => {
+        if (!v) return;
+        const {data:res} = await this.$http.post("/api/login", {
+          username: this.rulesForm.username,
+          password: this.rulesForm.password,
+        });
+        if(res.status!==200){
+          this.$message({
+            type:'error',
+            message:res.msg
+          })
+          return
+        }
+        sessionStorage.setItem('token',res.token)
+        this.$message({
+          type:'success',
+          message:res.msg
+        })
+      });
+    },
+  },
 };
 </script>
+
 
 <style scoped>
 .container {
@@ -139,14 +267,14 @@ a {
   position: absolute;
   top: 0;
   left: 0;
-  display:flex;
-  flex-direction: column;
-  justify-content: space-around;
   width: 100%;
   height: 100%;
   background: #f1f2f6;
-  padding: 10px;
+  padding: 20px 15px;
   box-sizing: border-box;
 }
-
+.login_btn {
+  margin-bottom: 15px;
+  margin-left: 5px;
+}
 </style>
